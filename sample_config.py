ae9588d4a1204623f057654a0cb745afffe3601e
blukwood <67267829+blukwood@users.noreply.github.com>
2021-10-18 16:35:29 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,8 +12,6 @@ if HEROKU:
     BOT_TOKEN = environ.get("BOT_TOKEN", None)
     API_ID = int(environ.get("API_ID", 6))
     API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    SESSION_STRING = environ.get("SESSION_STRING", None)
-    USERBOT_PREFIX = environ.get("USERBOT_PREFIX", ".")
     SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
     LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
     GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
@@ -22,14 +20,11 @@ if HEROKU:
     MONGO_URL = environ.get("MONGO_URL", None)
     ARQ_API_URL = environ.get("ARQ_API_URL", None)
     ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
-    LOG_MENTIONS = bool(int(environ.get("LOG_MENTIONS", None)))
     RSS_DELAY = int(environ.get("RSS_DELAY", None))
-    PM_PERMIT = bool(int(environ.get("PM_PERMIT", None)))
 else:
     BOT_TOKEN = "467677575:YZfaakjwd545dfg-N6JStihhuw5gQeZHntc"
     API_ID = 123456
     API_HASH = "dfxcgs5s12hdcxfgdfz"
-    USERBOT_PREFIX = "."
     PHONE_NUMBER = "+916969696969"  # Need for Userbot
     SUDO_USERS_ID = [
         4543744343,
@@ -42,6 +37,4 @@ else:
     MONGO_URL = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
     ARQ_API_KEY = "Get this from @ARQRobot"
     ARQ_API_URL = "https://thearq.tech"
-    LOG_MENTIONS = True
     RSS_DELAY = 300  # In seconds
-    PM_PERMIT = True
